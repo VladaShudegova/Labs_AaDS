afc2ed1164c689429a8a0845c0a14041b5b949a1
Vlada Shudegova <vlada.shudegova@gmail.com>
2024-02-29 11:22:04 +0700

    Add Qt and HashFunction

---
+++ HashFunction/Qt/HashTable.cpp
@@ -0,0 +1,213 @@
+﻿#include "HashTable.h"
+#include <iostream>
+#include <iomanip>
+
+HashTable::HashTable()
+{
+    m_nodeCount = 0;
+    setCapacity(10);
+    setHashFunction(new FirstHash());
+}
+
+
+HashTable::HashTable(int size)
+{
+    m_nodeCount = 0;
+    setCapacity(size);
+    setHashFunction(new FirstHash());
+}
+
+HashTable::HashTable(const HashTable& copy)
+{
+    m_nodeCount = 0;
+    setCapacity(copy.m_capacity);
+    setHashFunction(copy.m_hashFunction->clone());
+    copyTable(copy);
+}
+
+HashTable::~HashTable()
+{
+    clear();
+    delete m_hashFunction;
+}
+
+void HashTable::clear()
+{
+    for (Node* node : m_nodes)
+    {
+        while (node)
+        {
+            Node* previousNode = node;
+       
+            node = node->getNextNode();
+            delete previousNode;
+            
+        }
+    }
+    for (int i = 0; i < m_capacity; i++)
+    {
+        m_nodes[i] = nullptr;
+    }
+    m_nodeCount = 0;
+}
+
+int HashTable::add(int key, int value)
+{
+    unsigned int hash = m_hashFunction->hash(key, m_capacity);
+    Node* addNode = new Node(key, value);
+
+    if (m_nodes[hash] == nullptr)
+    {
+        m_nodes[hash] = addNode;
+    }
+    else
+    {
+        Node* temp = m_nodes[hash];
+        while (temp->getNextNode())
+        {
+            temp = temp->getNextNode();
+        }
+        temp->setNextNode(addNode);
+    }
+    m_nodeCount++;
+    return hash;
+}
+
+bool HashTable::remove(int key)
+{
+    unsigned int hash = m_hashFunction->hash(key, m_capacity);
+    Node* temp = m_nodes[hash];
+    Node* parent = nullptr;
+
+    while (temp != nullptr)
+    {
+        if (temp->getKey() == key)
+        {
+            if (parent)
+            {
+                parent->setNextNode(temp->getNextNode());
+                delete temp;
+            }
+            else
+            {
+                Node* nextNode = temp->getNextNode();
+                delete temp;
+                m_nodes[hash] = nextNode;
+            }
+            m_nodeCount--;
+            return true;
+        }
+        parent = temp;
+        temp = temp->getNextNode();
+    }
+    return false;
+}
+
+bool HashTable::contains(int key)
+{
+    unsigned int hash = m_hashFunction->hash(key, m_capacity);
+    Node* temp = m_nodes[hash];
+
+    while (temp)
+    {
+        if (temp->getKey() == key)
+        {
+            return true;
+        }
+
+        temp = temp->getNextNode();
+    }
+
+    return false;
+}
+
+void HashTable::printTable() const
+{
+    std::cout << "Hash" << std::setw(18) << "(Key, Value)" << std::setw(26) << "(_table[i], _next)" << std::endl;
+    for (int i = 0; i < m_capacity; i++) {
+        std::cout << "Hash " << i << ":" << std::setw(4);
+        Node* current = m_nodes[i];
+        if (current != nullptr) {
+            std::cout << std::setw(4) << "(" << current->getKey() << ", " << current->getValue() << ")";
+            std::cout << std::setw(9) << "(" << current << ", " << current->getNextNode() << ") ";
+            while (current->getNextNode())
+            {
+                current = current->getNextNode();
+                std::cout << std::setw(4) << "(" << current->getKey() << ", " << current->getValue() << ")";
+                std::cout << std::setw(9) << "(" << current << ", " << current->getNextNode() << ") ";
+            }
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void HashTable::copyTable(const HashTable& copy)
+{
+    clear();
+    m_nodeCount = copy.m_nodeCount;
+    setCapacity(copy.m_capacity);
+    setHashFunction(copy.m_hashFunction->clone());
+
+    for (Node* node : copy.nodes())
+    {
+        add(node->getKey(), node->getValue());
+    }
+}
+
+HashTable& HashTable::operator=(const HashTable& copy)
+{
+    if (this != &copy)
+    {
+        delete m_hashFunction;
+        copyTable(copy);
+    }
+    return *this;
+}
+
+int& HashTable::operator[](int key)
+{
+    if (contains(key))
+    {
+        throw "No key in table";
+    }
+
+    unsigned int hash = m_hashFunction->hash(key, m_capacity);
+    Node* temp = m_nodes[hash];
+
+    while (temp)
+    {
+        if (temp->getKey() == key)
+        {
+            return temp->GetAdressValue();
+        }
+
+        temp = temp->getNextNode();
+    }
+
+}
+
+void HashTable::changeHashFunction(HashFunction* newHashFunction) {
+    getisFirstHash() == true ? setfirstHash(false) : setfirstHash(true);
+
+
+    // Создаем временный вектор узлов для хранения всех элементов
+    std::vector<Node*> allNodes;
+    for (Node* node : m_nodes) {
+        while (node) {
+            allNodes.push_back(node);
+            node = node->getNextNode();
+        }
+    }
+
+    // Очищаем текущий список узлов
+    clear();
+
+    // Устанавливаем новую хеш-функцию
+    setHashFunction(newHashFunction);
+
+    // Переход по всем элементам, добавление их с использованием новой хеш-функции
+    for (Node* node : allNodes) {
+        add(node->getKey(), node->getValue());
+    }
+}
---
+++ HashFunction/Qt/mainwindow.cpp
@@ -0,0 +1,146 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QIntValidator>
+#include <QString>
+#include <QMessageBox>
+#include <iostream>
+#include <vector>
+#include <QDebug>
+
+MainWindow::MainWindow(int size, QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , table(size)
+{
+    ui->setupUi(this);
+    ui->keyEdit->setValidator(new QIntValidator {0, INT_MAX});
+    ui->keyEdit->setText("0");
+    ui->valueEdit->setValidator(new QIntValidator {0, INT_MAX});
+    ui->valueEdit->setText("0");
+    ui->keyForDelete->setValidator(new QIntValidator {0, INT_MAX});
+    ui->keyForDelete->setText("0");
+    ui->tableWidget->setColumnCount(1);
+
+    ui->tableWidget->setRowCount(table.getCapacity());
+    ui->tableWidget->setHorizontalHeaderLabels(QStringList() << "[Key, Value]");
+
+    QObject::connect(ui->addValueButton, &QPushButton::clicked, this, &MainWindow::on_clicked_addValue);
+    QObject::connect(ui->deleteValueButton, &QPushButton::clicked, this, &MainWindow::on_clicked_deleteValue);
+}
+
+void MainWindow::on_clicked_addValue()
+{
+    bool ok1;
+    int key = ui->keyEdit->text().toInt(&ok1);
+    bool ok2;
+    int value = ui->valueEdit->text().toInt(&ok2);
+
+    if(ok1 && ok2)
+    {
+        int hash = table.add(key, value);
+
+        std::cout << "table" << std::endl;
+        table.printTable();
+
+        QTableWidgetItem *existingItem = ui->tableWidget->item(hash, 0);
+
+
+        if (existingItem) {
+            QString existingData = existingItem->text();
+            existingData += QString(" [%1, %2]").arg(QString::number(key), QString::number(value));
+            ui->tableWidget->setItem(hash, 0, new QTableWidgetItem(existingData));
+        }
+        else {
+            ui->tableWidget->setItem(hash, 0, new QTableWidgetItem(QString("[%1, %2]").arg(QString::number(key), QString::number(value))));
+        }
+    }
+    else {
+        displayError(QString {"addValue: invalid key value: must be an integer"});
+    }
+    ui->deleteValueButton->setEnabled(true);
+}
+
+void MainWindow::on_clicked_deleteValue()
+{
+    bool ok;
+    int keyToDelete = ui->keyForDelete->text().toInt(&ok);
+    if(ok)
+    {
+        if(table.contains(keyToDelete))
+        {
+            FirstHash fHash;
+            HashFunction *hashFunction = &fHash;
+            int hash = hashFunction->hash(keyToDelete, table.getCapacity());
+
+            QTableWidgetItem *existingItem = ui->tableWidget->item(hash, 0);
+
+            if (existingItem) {
+                QString existingData = existingItem->text();
+                QStringList dataList = existingData.split(" ");
+                for (int i = 0; i < dataList.size(); ++i) {
+                    if (dataList[i].startsWith(QString("[%1,").arg(keyToDelete))) {
+//                        qDebug() << "Contents of dataList:";
+//                        for (const QString &item : dataList) {
+//                            qDebug() << item;
+//                        }
+                        dataList.removeAt(i+1); // Удаляем значение (пара идет [ключ, значение])
+                        dataList.removeAt(i);   // Удаляем ключ
+                        break;
+                    }
+                }
+                QString updatedData = dataList.join(" ");
+                ui->tableWidget->setItem(hash, 0, new QTableWidgetItem(updatedData));
+            }
+
+            table.remove(keyToDelete);
+        }
+        else {
+            displayError("deleteValue: this key not found");
+        }
+    }
+    else {
+        displayError("deleteValue: invalid key value: must be an integer");
+    }
+
+    table.printTable();
+}
+
+void MainWindow::displayError(QString const& msg)
+{
+    QMessageBox::warning(this, "error", msg);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+        // Получение значения свойства isHash у кнопки
+        //bool isHash = ui->pushButton->property("isHash").toBool();
+        bool isHash = table.getisFirstHash();
+        // Создание новой хеш-функции в зависимости от значения isHash
+        HashFunction* newHashFunction = nullptr;
+        if (isHash) {
+            newHashFunction = new FirstHash();
+
+            ui->pushButton->setText( "Change on Second hash");
+        } else {
+            newHashFunction = new SecondHash();
+
+            ui->pushButton->setText("Change on First hash");
+        }
+
+        // Смена хеш-функции для объекта table
+        table.changeHashFunction(newHashFunction);
+        ui->deleteValueButton->setEnabled(false);
+         table.printTable();
+        // Очистка выделенной памяти после использования
+        delete newHashFunction;
+
+}
+
